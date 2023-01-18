126f442023ec59ba75668abc552d720cc63b0d6a
quizee <quiz22295@gmail.com>
2023-01-18 22:44:04 +0900

    pagination 과 post 로직 수정, 광고란 수정

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import pymongo
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 client = MongoClient('mongodb+srv://test:sparta@cluster0.csmowq9.mongodb.net/Cluster0?retryWrites=true&w=majority')
@@ -8,19 +9,33 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route("/homework", methods=["POST"])
-def homework_post():
+@app.route("/blind", methods=["POST"])
+def blind_post():
     nick_name = request.form['nick_name']
-    comment = request.form['comment']
-    print(nick_name, comment)
-    doc = {'nick_name': nick_name, 'comment': comment}
-    db.guests.insert_one(doc)
+    content = request.form['content']
+    all_posts = list(db.posts.find({}, {'_id': False}))
+    count = len(all_posts) + 1
+    doc = {'nick_name': nick_name, 'content': content, 'post_id': count}
+    db.posts.insert_one(doc)
     return jsonify({'msg':'소중한 익명 의견 감사합니다!'})
 
-@app.route("/homework", methods=["GET"])
-def homework_get():
-    all_guests = list(db.guests.find({}, {'_id': False}))
-    return jsonify({'guests': all_guests})
+@app.route("/blind/comment", methods=["POST"])
+def blind_comment():
+    post_num = request.form['post_num']
+    comment = request.form['comment']
+    nick_name = request.form['nick_name']
+    doc = {'nick_name': nick_name, 'comment': comment, 'post_num': post_num}
+    db.comments.insert(doc)
+    return jsonify({'msg': '블라인드 창 생태계에 중독되셨군요!'})
+
+@app.route("/blind", methods=["GET"])
+def blind_get():
+    page = int(request.args.get('page', 1))
+    limit = 5
+    offset = (page -1)* limit
+    all_posts = list(db.posts.find({}, {'_id': False}).sort('post_id',pymongo.DESCENDING).limit(limit).skip(offset))
+    return jsonify({'posts': all_posts})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
